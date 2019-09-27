28e3b3f277bb435c4f229ce0af150bbff2b53fec
dgerard42 <daniella4422@gmail.com>
2019-09-26 21:59:06 -0700

    binary search for strings done, way easier than expected

--- 20190926/dgerard.cpp
+++ 20190926/dgerard.cpp
@@ -5,10 +5,8 @@
 using namespace std;
 
 const int SIZE_0 = 10;
-const int SIZE_1 = 20;
-const int SIZE_2 = 21;
 
-int		binary_search(int array[], int length, int search)
+int		binary_search(string array[], int length, string search)
 {
 	int	index = (length) / 2;
 	int	front = 0;
@@ -36,24 +34,18 @@ int		binary_search(int array[], int length, int search)
 
 int		main()
 {
-	string	test_0[SIZE_0] = {Avita, Brandon, Christian, Daniel, Nick, Rachel, ,8,9};
-	string	test_1[SIZE_1] = {0,1,1,2,2,3,4,5,6,9,13,17,20,20,21,22,32,37,41,42};
-	string	test_2[SIZE_2] = {-12,-12,-11,-3,1,2,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+	string	test_0[SIZE_0] = {"Aang", "Appa", "Azula", "Iroh", "Jet", "Katara", "Sokka", "Suki", "Toph", "Zuko"};
 
-	cout << binary_search(test_0, SIZE_0, 0) << "(0)" << endl;
-	cout << binary_search(test_0, SIZE_0, 1)  << "(1)" << endl;
-	cout << binary_search(test_0, SIZE_0, 2) << "(2)"  << endl;
-	cout << binary_search(test_0, SIZE_0, 3) << "(3)"  << endl;
-	cout << binary_search(test_0, SIZE_0, 4) << "(4)"  << endl;
-	cout << binary_search(test_0, SIZE_0, 5) << "(5)"  << endl;
-	cout << binary_search(test_0, SIZE_0, 6) << "(6)"  << endl;
-	cout << binary_search(test_0, SIZE_0, 7)  << "(7)" << endl;
-	cout << binary_search(test_0, SIZE_0, 8) << "(8)"  << endl;
-	cout << binary_search(test_0, SIZE_0, 9) << "(9)"  << endl;
-	cout << binary_search(test_0, SIZE_0, 10) << "(-1)"  << endl;
-	cout << binary_search(test_0, SIZE_0, -1) << "(-1)"  << endl;
-	cout << binary_search(test_2, SIZE_2, -12) << "(0 || 1))"  << endl;
-	cout << binary_search(test_2, SIZE_2, 16)  << "(20)" << endl;
-	cout << binary_search(test_1, SIZE_1, 32)  << "(16)" << endl;
+	cout << binary_search(test_0, SIZE_0, "Aang") << "(0)" << endl;
+	cout << binary_search(test_0, SIZE_0, "Appa")  << "(1)" << endl;
+	cout << binary_search(test_0, SIZE_0, "Azula") << "(2)"  << endl;
+	cout << binary_search(test_0, SIZE_0, "Iroh") << "(3)"  << endl;
+	cout << binary_search(test_0, SIZE_0, "Jet") << "(4)"  << endl;
+	cout << binary_search(test_0, SIZE_0, "Katara") << "(5)"  << endl;
+	cout << binary_search(test_0, SIZE_0, "Sokka") << "(6)"  << endl;
+	cout << binary_search(test_0, SIZE_0, "Suki")  << "(7)" << endl;
+	cout << binary_search(test_0, SIZE_0, "Toph") << "(8)"  << endl;
+	cout << binary_search(test_0, SIZE_0, "Zuko") << "(9)"  << endl;
+	cout << binary_search(test_0, SIZE_0, "Beatrice") << "(-1)"  << endl;
 	return 0;
 }
